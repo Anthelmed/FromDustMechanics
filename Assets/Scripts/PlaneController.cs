6647c4d668a016d4a9387549300fd0c85115d2a8
anthelme dumont <anthelme.dumont@rappfrance.com>
2016-12-21 16:55:05 +0100

    update texture painting

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -7,12 +7,14 @@ public class PlaneController : MonoBehaviour {
 
 	public MeshFilter meshFilter;
 	public MeshRenderer meshRenderer;
+	public MeshCollider meshCollider;
 	[Range(32, 248)]
-	public int size = 62;
+	public int segments = 62;
 
 	void Update () {
-		MeshData meshData = MeshGenerator.GenerateTerrainMesh (size, size);
+		MeshData meshData = MeshGenerator.GenerateTerrainMesh (segments, segments);
 
 		meshFilter.sharedMesh = meshData.CreateMesh ();
+		meshCollider.sharedMesh = meshFilter.sharedMesh;
 	}
 }
